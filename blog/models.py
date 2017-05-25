61f645862f1d8f4a56f28fa8684a3e0f65a87dd8
Alex Miller <alex.k.miller@gmail.com>
2017-05-24 21:16:35 -0400

    Pagination

--- blog/models.py
+++ blog/models.py
@@ -16,17 +16,52 @@ from wagtail.wagtailsearch import index
 
 from wagtail.wagtailsnippets.models import register_snippet
 
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 
 class BlogIndexPage(Page):
     parent_page_types = ['home.HomePage']
     subpage_types = ['blog.BlogPage']
     
+    @property
+    def blogs(self):
+        # Get list of live blog pages that are descendants of this page
+        blogs = BlogPage.objects.live().descendant_of(self)
+
+        # Order by most recent date first
+        blogs = blogs.order_by('-date')
+
+        return blogs
+
     def get_context(self, request):
-        # Update context to include only published posts, ordered by reverse-chron
+        # Get blogs
+        blogs = self.blogs
+
+        # Filter by tag
+        tag = request.GET.get('tag')
+        if tag:
+            blogs = blogs.filter(tags__name=tag)
+
+        # Pagination
+        page = request.GET.get('page')
+        paginator = Paginator(blogs, 5)  # Show 10 blogs per page
+        try:
+            blogs = paginator.page(page)
+        except PageNotAnInteger:
+            blogs = paginator.page(1)
+        except EmptyPage:
+            blogs = paginator.page(paginator.num_pages)
+
+        # Update template context
         context = super(BlogIndexPage, self).get_context(request)
-        blogpages = self.get_children().live().order_by('-first_published_at')
-        context['blogpages'] = blogpages
+        context['blogs'] = blogs
         return context
+        
+        content_panels = [
+            FieldPanel('title', classname="full title"),
+        ]   
+
+        promote_panels = Page.promote_panels
 
 class BlogPageTag(TaggedItemBase):
     content_object = ParentalKey('BlogPage', related_name='tagged_items')
@@ -111,6 +146,11 @@ class BlogPage(Page):
     promote_panels = Page.promote_panels + [
         ImageChooserPanel('feed_image'),
     ]
+    
+    @property
+    def blog_index(self):
+        # Find closest ancestor which is a blog index
+        return self.get_ancestors().type(BlogIndexPage).last()
 
     
 class BlogPageGalleryImage(Orderable):
@@ -124,19 +164,6 @@ class BlogPageGalleryImage(Orderable):
         ImageChooserPanel('image'),
         FieldPanel('caption'),
     ]
-    
-class BlogTagIndexPage(Page):
-
-    def get_context(self, request):
-
-        # Filter by tag
-        tag = request.GET.get('tag')
-        blogpages = BlogPage.objects.filter(tags__name=tag).live()
-
-        # Update template context
-        context = super(BlogTagIndexPage, self).get_context(request)
-        context['blogpages'] = blogpages
-        return context
     
 @register_snippet
 class BlogCategory(models.Model):
